d761727971d7c1d4d88d56554f49c1dcaa01601f
Iulia Ilie <iulia@oc8345618802.ibm.com>
2020-01-31 11:28:02 +0200

    group in angular modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DoctorHomeComponent } from './doctor-pages/doctor-home/doctor-home.component';
+import { DoctorHomeComponent } from './doctor/doctor-home/doctor-home.component';
 import { LoginComponent } from './login/login.component';
-import { PatientListComponent } from './doctor-pages/patient-list/patient-list.component';
-import { SettingsComponent } from './doctor-pages/settings/settings.component';
-import { CalendarComponent } from './doctor-pages/calendar/calendar.component';
-import { PatientSearchComponent } from './doctor-pages/patient-search/patient-search.component';
-import { CreatePatientComponent } from './doctor-pages/create-patient/create-patient.component';
-import { CreateConsultationComponent } from './doctor-pages/create-consultation/create-consultation.component';
-import { PatientAlertsComponent } from './doctor-pages/patient-alerts/patient-alerts.component';
-import { MedicalHistoryComponent } from './medical-history/medical-history.component';
+import { PatientListComponent } from './patients/patient-list/patient-list.component';
+import { SettingsComponent } from './doctor/settings/settings.component';
+import { CalendarComponent } from './consultations/calendar/calendar.component';
+import { PatientSearchComponent } from './patients/patient-search/patient-search.component';
+import { CreatePatientComponent } from './patients/create-patient/create-patient.component';
+import { CreateConsultationComponent } from './consultations/create-consultation/create-consultation.component';
+import { PatientAlertsComponent } from './patients/patient-alerts/patient-alerts.component';
+import { MedicalHistoryComponent } from './patients/medical-history/medical-history.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'doctor-home', component: DoctorHomeComponent },
-  { path: 'patients-list', component: PatientListComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'search', component: PatientSearchComponent },
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'doctor-home',
+    component: DoctorHomeComponent
+  },
+  {
+    path: 'patients-list',
+    component: PatientListComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
+  },
+  {
+    path: 'search',
+    component: PatientSearchComponent
+  },
   {
     path: 'create-patient',
     component: CreatePatientComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { DoctorHomeComponent } from './doctor-pages/doctor-home/doctor-home.component';
+import { DoctorHomeComponent } from './doctor/doctor-home/doctor-home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PatientListComponent } from './doctor-pages/patient-list/patient-list.component';
-import { SettingsComponent } from './doctor-pages/settings/settings.component';
-import { CalendarComponent } from './doctor-pages/calendar/calendar.component';
-import { PatientAlertsComponent } from './doctor-pages/patient-alerts/patient-alerts.component';
-import { CreateConsultationComponent } from './doctor-pages/create-consultation/create-consultation.component';
-import { PatientSearchComponent } from './doctor-pages/patient-search/patient-search.component';
-import { PatientsAllComponent } from './doctor-pages/patients-all/patients-all.component';
-import { CreatePatientComponent } from './doctor-pages/create-patient/create-patient.component';
+import { PatientListComponent } from './patients/patient-list/patient-list.component';
+import { SettingsComponent } from './doctor/settings/settings.component';
+import { CalendarComponent } from './consultations/calendar/calendar.component';
+import { PatientAlertsComponent } from './patients/patient-alerts/patient-alerts.component';
+import { CreateConsultationComponent } from './consultations/create-consultation/create-consultation.component';
+import { PatientSearchComponent } from './patients/patient-search/patient-search.component';
+import { PatientsAllComponent } from './patients/patients-all/patients-all.component';
+import { CreatePatientComponent } from './patients/create-patient/create-patient.component';
 import { CalendarModule } from 'primeng/calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -23,7 +23,7 @@ import { ButtonModule } from 'primeng/primeng';
 import { RadioButtonModule } from 'primeng/primeng';
 import { AccordionModule } from 'primeng/accordion';
 import { ToastrModule } from 'ngx-toastr';
-import { MedicalHistoryComponent } from './medical-history/medical-history.component';
+import { MedicalHistoryComponent } from './patients/medical-history/medical-history.component';
 
 @NgModule({
   declarations: [
