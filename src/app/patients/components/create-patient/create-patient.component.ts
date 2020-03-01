4835254a5d87695dd919a7c13bdc14baec1786bf
Iulia Ilie <iulia@oc8345618802.ibm.com>
2020-03-01 22:05:57 +0200

    required error message for create

--- src/app/patients/components/create-patient/create-patient.component.ts
+++ src/app/patients/components/create-patient/create-patient.component.ts
@@ -6,6 +6,7 @@ import { ToastrService } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { SavePatientDto } from '../../model/SavePatientDto';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-create-patient',
@@ -14,24 +15,36 @@ import { SavePatientDto } from '../../model/SavePatientDto';
 })
 export class CreatePatientComponent implements OnInit {
 
+  errors = "";
   patient: PatientDto = new PatientDto();
   savePatientDto: SavePatientDto = new SavePatientDto();
   address: AddressDto = new AddressDto();
   pipe = new DatePipe('en-us');
-  constructor(private patientApi: PatientApiService, private router: Router, private toastr: ToastrService) { }
+  constructor(private patientApi: PatientApiService, private router: Router, private translateService: TranslateService, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
   save() {
+    if (this.validate()) {
+      this.patient.address = this.address;
+      this.patient.birthdate = this.pipe.transform(this.patient.birthdate, 'yyyy-MM-dd');
+      this.savePatientDto.patient = this.patient;
+      this.savePatientDto.doctorUsername = sessionStorage.getItem("loggedDoctor");
+      this.patientApi.savePatient(this.savePatientDto).subscribe(patientId =>
+        this.router.navigate(['patient', 'create-patient', 'create-medical-chart', patientId])
+      );
+    }
+  }
 
-    this.patient.address = this.address;
-    this.patient.birthdate = this.pipe.transform(this.patient.birthdate, 'yyyy-MM-dd');
-    this.savePatientDto.patient = this.patient;
-    this.savePatientDto.doctorUsername = sessionStorage.getItem("loggedDoctor");
-    this.patientApi.savePatient(this.savePatientDto).subscribe(patientId =>
-      this.router.navigate(['patient', 'create-patient', 'create-medical-chart', patientId])
-    );
+  validate() {
+    if (!(this.patient.name && this.patient.lastname && this.patient.birthdate &&
+      this.patient.email && this.patient.phone && this.address.addressLine1 &&
+      this.address.city && this.address.country && this.address.region)) {
+      this.toastr.error(this.translateService.instant("error.fields.required"))
+      return false;
+    }
+    return true;
   }
 
 }
