236f522928ae1692d4cf956f1edf1d02d7f402f1
Iulia Ilie <iulia@oc8345618802.ibm.com>
2020-06-22 15:10:52 +0300

    add errors from backedn

--- src/app/shared/service/http-interceptor.ts
+++ src/app/shared/service/http-interceptor.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HTTP_INTERCEPTORS
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class HttpRequestsInterceptor implements HttpInterceptor {
 
-  constructor(private toasterService: ToastrService) { }
+  constructor(private toasterService: ToastrService, private translateService: TranslateService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((err: any) => {
-
-        console.log(err)
         if (err instanceof HttpErrorResponse) {
-          this.toasterService.error('An error occurred:' + err.error.errorCode);
+          if (err.error) {
+            let code = JSON.parse(err.error);
+            this.toasterService.error(this.translateService.instant(code.errorCode));
+          }
+          else{
+            this.toasterService.error(this.translateService.instant('backend.generic'))
+          }
 
         }
         return of(err);
